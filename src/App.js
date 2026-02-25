8891f02767a3cd89693e4b44ed80b738381775e6
Maria <mariasaldanha124@gmail.com>
2026-02-25 01:57:00 -0300

    Aplicando UseState e UseEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Realizar as tarefas',
+  ]);
+
+  useEffect(() => { // se der f5, a lista fica salva
+    const tarefasStorage = localStorage.getItem('@tarefa');
+    // foi no localStorage, buscou se tem alguma coisa e se tem,
+    // colocou na UseState
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+  },[]);
+
+  useEffect(() => { // vai salvar a tarefa que foi registrada no LocalStorage
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas))
+  }, [tarefas]);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando o nosso usuário</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa:</label><br/>
+        <input 
+          placeholder='Digite uma tarefa'
+          value={input}
+          onChange={ (e) => setInput(e.target.value) }
+        /><br/>
+        <button type='submit'>Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>
+            {tarefa}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
